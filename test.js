d26d4ef4a5aca71cb03ddbe4105a7f78ac465d1f
Mirza Mehedi Iqbal <mirza@therapbd.net>
2025-01-13 12:23:37 +0600

    new class

--- test.js
+++ test.js
@@ -1,2 +1,15 @@
 console.log(!!"");
 console.log(!!"false");
+
+class Car {
+    constructor(model, attributes){
+        this.model = model;
+        this.attributes= attributes;
+    }
+
+    
+}
+
+const myCar = new Car("toyota","high Speed");
+
+console.log(myCar.attributes);
